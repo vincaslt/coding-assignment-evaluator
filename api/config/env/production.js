6f1b0c47a9587b44704ef95a9ac5bdd9c759c40d
Vincas Stonys <vincas.stonys@gmail.com>
2017-01-30 23:57:16 +0200

    Add openshift config

--- api/config/env/production.js
+++ api/config/env/production.js
@@ -1,6 +1,7 @@
 export default {
   env: 'production',
-  db: 'mongodb://localhost/coding-production',
-  port: 4040,
+  db: process.env.OPENSHIFT_MONGODB_DB_URL,
+  port: process.env.OPENSHIFT_NODEJS_PORT,
+  host: process.env.OPENSHIFT_NODEJS_IP,
   adminPassword: 'bvzRTaz21x'
 }
